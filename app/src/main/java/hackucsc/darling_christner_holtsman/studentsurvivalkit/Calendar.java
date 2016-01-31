f36ab480d602cf4b3d62a24567061d5f9245dda4
Bailey <bails635@gmail.com>
2016-01-30 16:34:32 -0800

    Improved Calendar useability Calendar functions implemented Marc's reg implemented

--- app/src/main/java/hackucsc/darling_christner_holtsman/studentsurvivalkit/Calendar.java
+++ app/src/main/java/hackucsc/darling_christner_holtsman/studentsurvivalkit/Calendar.java
@@ -1,13 +1,60 @@
 package hackucsc.darling_christner_holtsman.studentsurvivalkit;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import org.w3c.dom.Text;
 
 public class Calendar extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+
+        setup();
+
+    }
+
+    int year;
+    int month;
+    int day;
+    boolean isEven;
+
+    public void setup() {
+        final CalendarView cv = (CalendarView) findViewById(R.id.calendarView);
+        cv.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int grabyear, int grabmonth, int dayOfMonth) {
+                if(dayOfMonth%2 == 0){
+                   cv.setSelectedWeekBackgroundColor(-16776961);
+                }else{
+                    cv.setSelectedWeekBackgroundColor(-65536);
+                }
+
+                String day = Cal_Proccess.getDay(cv, view);
+                TextView tx = (TextView) findViewById(R.id.test);
+                tx.setText(day);
+                //year = grabyear;
+               // month = grabmonth;
+                //day = dayOfMonth;
+            }
+        });
+
     }
 }
--- app/src/main/java/hackucsc/darling_christner_holtsman/studentsurvivalkit/Home.java
+++ app/src/main/java/hackucsc/darling_christner_holtsman/studentsurvivalkit/Home.java
@@ -5,11 +5,16 @@ package hackucsc.darling_christner_holtsman.studentsurvivalkit;
 //
 //
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Home extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +22,7 @@ public class Home extends AppCompatActivity {
         setContentView(R.layout.activity_home);
     }
 
-   public void calPress(View view){
+    public void calPress(View view){
         Intent intent= new Intent (this, Calendar.class);
         startActivity(intent);
     }
@@ -25,5 +30,10 @@ public class Home extends AppCompatActivity {
     public void gradeTrack(View view){
         Intent intent = new Intent (this, GradeTracker.class);
         startActivity(intent);
+    }
+
+    public void registerClass(View v){
+        Intent registerIntent = new Intent(this, Registration_activity.class);
+        startActivity(registerIntent);
     }
 }
